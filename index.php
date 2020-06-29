90f6040396be536b712d1905a9d9d8d4c7f89e82
sebastien juchet <dyonisos40games@gmail.com>
2020-06-29 10:39:48 +0200

    add error page and try/catch for error management

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <?php 
 require 'controler/Main.php';
 session_start();
-if(empty($_GET)) {
-    require_once 'view/slider.php';
-} 
-if (isset($_GET['action'])) {
-    if ($_GET['action'] === 'listeChapitres') {
-        listPosts();
-    } elseif ($_GET['action'] === 'post' && isset($_GET['id']) && $_GET['id'] > 0) {
-        showPost((int) $_GET['id']);
-    } elseif ($_GET['action'] === 'addComment') {
-        $postId = $_GET['id'];
-        $author = htmlspecialchars($_POST['author']);
-        $comment = htmlspecialchars($_POST['comment']);
-        $reportComment = false;
-        $_SESSION['author'] = $author;
-        
-        if (!empty($author) && !empty($comment)) {
-            createComments($postId, $author, $comment, $reportComment);
-        } else {
+try {
+    if(empty($_GET)) {
+        require_once 'view/slider.php';
+    } 
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] === 'listeChapitres') {
+            listPosts();
+        } elseif ($_GET['action'] === 'post' && isset($_GET['id']) && $_GET['id'] > 0) {
             showPost((int) $_GET['id']);
-        }    
-    }   
-} 
+        } elseif ($_GET['action'] === 'addComment') {
+            $postId = $_GET['id'];
+            $author = htmlspecialchars($_POST['author']);
+            $comment = htmlspecialchars($_POST['comment']);
+            $reportComment = false;
+            $_SESSION['author'] = $author;
+            
+            if (!empty($author) && !empty($comment)) {
+                createComments($postId, $author, $comment, $reportComment);
+            } else {
+                throw new Exception('Vous n\'avez pas remplis les champs demandés!!!');
+            }           
+        }   
+    } 
+}
+
+catch (Exception $exception) {
+    require_once 'view/viewError.php';
+}   
